677d5d8a7ce3175dfdcb4763db68b77005928676
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-04-02 14:24:21 +0800

    新增 /hello 路由

--- app.py
+++ app.py
@@ -12,3 +12,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/hello')
+def hello()
+    return '<h1>Hello Flask</h1>'
+
+if __name__ == '__main__':
+    app.run()
