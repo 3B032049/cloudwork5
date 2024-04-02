1c04606d71a057844ec1b913913a1ba1aa948ccd
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-04-02 14:41:48 +0800

    字串return調整

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def hello():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,{name}!</h1>'
+    return f'<h1>Hello,{name}!</h1>'
 
 @app.route('/user/<name>/<surname>')
 def user_surname(name,surname):
-    return '<h1>Hello,{name}{surname}<!/h1>'
+    return f'<h1>Hello,{name}{surname}<!/h1>'
 
 if __name__ == '__main__':
     app.run()
