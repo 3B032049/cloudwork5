d0b7ecbeb9bc92de16a0246a48d3bc94b94acaac
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-04-02 14:32:11 +0800

    動態路由設定

--- app.py
+++ app.py
@@ -17,5 +17,13 @@ def index():
 def hello():
     return '<h1>Hello Flask</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,{}!</h1>'.format(name)
+
+@app.route('/user/<name>/<surname>')
+def user(name,surname):
+    return '<h1>Hello,{}{}<!/h1>'.format(name,surname)
+
 if __name__ == '__main__':
     app.run()
