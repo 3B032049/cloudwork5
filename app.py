3478927741f4174ce48ad2cee6468e4daa4ca438
鄭哲丞 <a0968518713@gmail.com>
2024-04-30 13:59:11 +0800

    新增shopping ticket welfare .html

--- app.py
+++ app.py
@@ -7,15 +7,24 @@ Created on Tue Mar 26 15:04:03 2024
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates',
+            static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return '<h1>Hello Flask</h1>'
+@app.route('/shopping')
+def shopping():
+    return render_template('shopping.html')
+
+@app.route('/ticket')
+def ticket_saling():
+    return render_template('ticket_saling.html')
+
+@app.route('/welfare')
+def welfare_platform():
+    return render_template('welfare_platform.html')
 
 @app.route('/user/<name>')
 def user(name):
