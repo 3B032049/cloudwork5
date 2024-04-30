e0ef3f8c8036ac78ed0ffbd304c865b5e96eba90
鄭哲丞 <a0968518713@gmail.com>
2024-04-30 14:11:28 +0800

    Flask GET 取得參數的方式

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ Created on Tue Mar 26 15:04:03 2024
 @author: user
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template ,request
 
 app = Flask(__name__, template_folder='templates',
             static_url_path='/static', static_folder='static')
 
 @app.route('/')
+@app.route('/index',methods=['GET'])
 def index():
-    return render_template('index.html')
+    name = request.args.get('name')
+    return render_template('index.html', **locals())
 
 @app.route('/shopping')
 def shopping():
