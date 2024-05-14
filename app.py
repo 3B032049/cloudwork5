816d475c41e07ecaacd2705f49c745ab3ef03012
鄭哲丞 <a0968518713@gmail.com>
2024-05-14 13:51:37 +0800

    帳號資料表

--- app.py
+++ app.py
@@ -6,11 +6,26 @@ Created on Tue Mar 26 15:04:03 2024
 """
 
 from flask import Flask, render_template ,request
-from flask import redirect, url_for
+from flask import redirect, url_for, session
+import hashlib
+import psycopg2
+import dbconn
 
 app = Flask(__name__, template_folder='templates',
             static_url_path='/static', static_folder='static')
+app.secret_key = 'fd4723e200261a2271ea912571eaaald'
+
 name = ''
+
+# DB Connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=dbconn.host,
+        database=dbconn.database,
+        user=dbconn.user,
+        password=dbconn.password)
+    return conn
+
 @app.route('/')
 @app.route('/index',methods=['GET'])
 def index():
@@ -29,21 +44,49 @@ def ticket():
 def welfare():
     return render_template('welfare.html')
 
-@app.route('/user/<username>', methods=["GET"])
+'''@app.route('/user/<username>', methods=["GET"])
 def user(username):
     message= request.args.get("message")
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name)'''
+
+@app.route('/user')
+def user():
+     if 'username' in session:
+        username = session['username']
+        return render_template('user.html', name=username)
+    else:
+        return redirect(url_for('signin'))
 
 @app.route('/member/signin')
 def signin():
     return render_template('member/signin.html')
-@app.route('/member/login', methods=["POST"])
+'''@app.route('/member/login', methods=["POST"])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         userpassword = request.form['userpassword']
         message = '登入成功!'
         return redirect(url_for('user', username=username, message=message))
+    else:
+        render_template("member/signin.html")'''
+@app.route('/member/login', methods=["POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        userpass = request.form['userpassword']
+        md = hashlib.md5()
+        md.update(userpass.encode('utf-8'))
+        hashpass = md.hexdigest()
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        SQL = "SELECT username, userpass FROM account WHERE username='{username}';"
+        cursor.execute (SQL)
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if (username == user [0] and hashpass == user[1]):
+            session['username'] = username
+            return redirect(url_for('user'))
     else:
         render_template("member/signin.html")
 
@@ -55,8 +98,10 @@ def user(username):
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    global name
-    username = name
+    '''global name
+    username = name'''
+    if 'username' in session:
+        username = session['username']
     if request.method =='POST':
         keyword = request.values['keyword']
         if keyword == '紅燈':
---
+++ dbconn.py
@@ -0,0 +1,4 @@
+host = 'dpg-cojk7o8cmk4c73bst0n0-a.singapore-postgres.render.com'
+database = 'smarthome_b23q'
+user = 'admin'
+password = 'ZcZBH2wWJBvlh2MrlrnGdt8lRTlYyA1r'
